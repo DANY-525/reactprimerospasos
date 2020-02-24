23ce9f37179a5a8e521726dcfcae27cbb5fcbac6
dany525@gmail.com <dany525@gmail.com>
2020-02-23 20:08:23 -0500

    react primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Logica from './Logica';
 
 function App() {
+
+  const sarHello = () => {
+
+    console.log("hello here");
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+    <Logica name ="Dev Ed" />
+     <Logica name ="jhon snow" />
+     <Logica name ="daniel Saavedra" />
     </div>
   );
 }
---
+++ src/Logica.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+
+import './App.css'
+
+function Logica(){
+
+
+
+    return(
+        <div className="logica">
+            <h3>name</h3>
+
+            <p>this is the last remenber</p>
+
+            <h3>number of likes</h3>
+
+
+        </div>
+
+
+    );
+}
+
+export default Logica;
